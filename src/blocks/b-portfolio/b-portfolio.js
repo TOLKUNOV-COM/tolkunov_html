12f38ce9e874565034e2d078e824de3bd64db38d
Michael <michlenanosoft@gmail.com>
2016-10-28 09:37:10 +0500

    done portfolio modal view

---
+++ src/blocks/b-portfolio/b-portfolio.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.b-portfolio__item').fancybox({
+        type: 'inline',
+        openEffect: 'none',
+        closeEffect: 'none',
+        padding: [50, 0, 0, 0],
+        margin: [74, 0, 0, 0],
+        fitToView: false,
+        width: 1082
+        //tpl: {
+        //    image: '<div class="b-review-image"><div class="b-review-image__container"><img class="" src="{href}" alt="" /></div></div>'
+        //}
+    });
+});
