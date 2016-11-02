6858d87e0439813a212d8df733ab42e1a23b9145
Michael <michlenanosoft@gmail.com>
2016-11-03 01:21:15 +0500

    add price-modal block

---
+++ src/blocks/b-prices/b-prices.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.b-prices__link').fancybox({
+        type: 'inline',
+        openEffect: 'none',
+        closeEffect: 'none',
+        padding: 0,
+        margin: [74, 0, 0, 0],
+        fitToView: false
+    });
+});
