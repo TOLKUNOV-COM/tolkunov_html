19e787eba40fcf56bb810a3a586cdf1c21a02f86
Michael <michlenanosoft@gmail.com>
2017-04-12 10:08:06 +0500

    add vacancy text add summary block to splash page refactor offers

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,2 +1,19 @@
 $(function () {
+    $('.js-fancybox-video').fancybox({
+        openEffect: 'none',
+        closeEffect: 'none',
+        padding: 0,
+        margin: [74, 0, 0, 0],
+        helpers: {
+            media: {}
+        }
+    });
+    $('.js-fancybox-inline').fancybox({
+        type: 'inline',
+        openEffect: 'none',
+        closeEffect: 'none',
+        padding: 0,
+        margin: [74, 0, 0, 0],
+        fitToView: false
+    });
 });
