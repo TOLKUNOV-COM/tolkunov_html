80f03a0ba9d5cffba2d39dc12bb95658f67ba014
Michael <michlenanosoft@gmail.com>
2018-10-27 02:55:24 +0500

    fix portfolio title size fancybox margin fix

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -9,7 +9,7 @@ $(function () {
         openEffect: 'none',
         closeEffect: 'none',
         padding: 0,
-        margin: [74, 0, 0, 0],
+        margin: [168, 0, 100, 0],
         helpers: {
             media: {}
         },
@@ -31,7 +31,7 @@ $(function () {
         openEffect: 'none',
         closeEffect: 'none',
         padding: 0,
-        margin: [74, 0, 0, 0],
+        margin: [168, 0, 100, 0],
         fitToView: false
     });
 
