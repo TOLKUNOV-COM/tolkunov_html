0b20e77c1a20d3a0d2ee6cff2cbc3089942376d8
Michael <michlenanosoft@gmail.com>
2018-11-05 06:31:41 +0500

    add callback widget some other fixes revert portfolio_item "other" block

---
+++ src/blocks/b-call-widget/b-call-widget.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('.b-call-widget__button').on('click', function () {
+        if (!$('.b-call-widget').hasClass('b-call-widget__collapsed')) {
+            setTimeout(function () {
+                $('.b-call-widget__content').css('display', 'none');
+            }, 300);
+        } else {
+            $('.b-call-widget__content').css('display', 'block');
+        }
+
+        setTimeout(function () {
+            $('.b-call-widget').toggleClass('b-call-widget__collapsed');
+        }, 1);
+    });
+});
