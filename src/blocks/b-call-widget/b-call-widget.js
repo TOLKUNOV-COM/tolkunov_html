4548a680b5967f6d1945c9f25a58e2f423f8a02a
Michael <michlenanosoft@gmail.com>
2019-08-29 00:51:05 +0500

    add widget call-to-action button new design changes (aug 19) update gulp-less package

--- src/blocks/b-call-widget/b-call-widget.js
+++ src/blocks/b-call-widget/b-call-widget.js
@@ -1,11 +1,30 @@
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
+
+$(function () {
+    /* When iframe is loaded */
+    $('#callbackFrame').ready(function () {
+        setTimeout(function () {
+            $('.b-call-widget').addClass('b-call-widget_state_ready');
+            $('.b-call-widget__content').css('display', 'none');
+            $('.b-call-widget__overlay').css('display', 'none');
+        }, 1);
+    });
+
+    if (!isMobileDevice()) {
+        $('.b-call-widget').addClass('b-call-widget_without-overlay');
+    }
+});
+
 $(function () {
     $('.b-call-widget__button').on('click', function () {
         if (!$('.b-call-widget').hasClass('b-call-widget__collapsed')) {
             setTimeout(function () {
-                $('.b-call-widget__content').css('display', 'none');
+                $('.b-call-widget__content, .b-call-widget__overlay').css('display', 'none');
             }, 500);
         } else {
-            $('.b-call-widget__content').css('display', 'block');
+            $('.b-call-widget__content, .b-call-widget__overlay').css('display', 'block');
         }
 
         setTimeout(function () {
