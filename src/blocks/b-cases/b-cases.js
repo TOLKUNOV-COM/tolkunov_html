70c994406701fd976af3722a89ccd43b1084d768
Mikhail Chemezov <michlenanosoft@gmail.com>
2022-10-24 15:32:26 +0500

    new cases view. added sample videos.

--- src/blocks/b-cases/b-cases.js
+++ src/blocks/b-cases/b-cases.js
@@ -1,13 +1,13 @@
 var loadCases = function (cb) {
-    $('.b-cases__list').each(function () {
-        let itemsCount = $(this).find('.b-cases__item').length;
-
-        let mod2 = itemsCount % 2;
-        let mod3 = itemsCount % 3;
-
-        $(this).addClass('b-cases__list_mod_2_' + mod2);
-        $(this).addClass('b-cases__list_mod_3_' + mod3);
-    });
+    // $('.b-cases__list').each(function () {
+    //     let itemsCount = $(this).find('.b-cases__item').length;
+    //
+    //     let mod2 = itemsCount % 2;
+    //     let mod3 = itemsCount % 3;
+    //
+    //     $(this).addClass('b-cases__list_mod_2_' + mod2);
+    //     $(this).addClass('b-cases__list_mod_3_' + mod3);
+    // });
 
     cb && cb();
 
@@ -16,16 +16,19 @@ var loadCases = function (cb) {
 };
 
 var loadCasesListVideo = function () {
-    $('.js-cases-cover-video').each(function () {
+    $('.js-cases-cover-video:visible').each(function () {
         // Check for already loaded
         if ($(this).next('video').length) {
-            return false;
+            return true;
         }
+
+        let ratio = $(this).data('ratio');
 
         var $video = $('<video>')
             .attr('preload', 'auto')
             .attr('playsinline', 'playsinline')
             .addClass('b-cases__video')
+            .addClass('b-cases__video_' + ratio)
             .hide();
 
         let src = $(this).data('src');
@@ -59,5 +62,9 @@ var loadCasesListVideo = function () {
 };
 
 window.addEventListener('load', function () {
+    loadCases();
+});
+
+$(window).on('resize', function () {
     loadCases();
 });
